a1df4dfa1a538b088bc9a621bf1f29bf4a41e3a5
Paula4884 <162999918+Paula4884@users.noreply.github.com>
2024-09-30 10:38:37 -0300

    Add files via upload

--- masciotra_web/inicio_sesion/proceso.php
+++ masciotra_web/inicio_sesion/proceso.php
@@ -1,13 +1,19 @@
-<link rel="stylesheet" href="style.css">
-
 <?php
 
 $conex = mysqli_connect("localhost:3306","root","","registro");
 
 ?>
 
-
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../css/style.css">
+	<title>Document</title>
+</head>
+<body class="reb">
+	<?php 
 
 if (isset($_POST['register'])) {
     if (strlen($_POST['mail']) >= 1 && strlen($_POST['pass']) >= 1) {
@@ -18,8 +24,28 @@ if (isset($_POST['register'])) {
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
 	    	?> 
-	    	<h1 class="ok">¡Has inicidado sesión correctamente!</h1>
-            <a class="aphp" href="../index.html">Volver a Inicio</a>
+                 <header>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <a href="../index.html" class="logo">
+        <img src="../img/logo_mm.png" alt="logo de la compañia" class="logo-img">
+        <h2 class="logo-nombre">Mascio Music</h2>
+      </a>
+      <ul>
+        <li><a href="../index.html">Inicio</a></li>
+        <li><a href="../productos/index.html">Productos</a></li>
+        <li><a href="../eventos/index.html">Evento</a></li>
+        <li><a href="../inicio_sesion/index.html">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>			
+                <h1 class="ok">¡Has iniciado sesión correctamente!</h1>
+				<br>
+				<br>
+				<a class="aphp" href="../index.html">VOLVER A INICIO</a>
            <?php
 	    } else {
 	    	?> 
@@ -29,6 +55,12 @@ if (isset($_POST['register'])) {
     }   else {
 	    	?> 
 	    	<h1 class="bad">¡Por favor complete los campos!</h1>
+			<a href="../inicio_sesion/"></a>
            <?php
     }
 }
+?>
+
+</body>
+</html>
+
